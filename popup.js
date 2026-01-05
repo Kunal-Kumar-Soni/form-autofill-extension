a3c8099192badfcbae259e329aafef5481d30e1f
Kunal <kunal.codes24@gmail.com>
2026-01-05 16:35:17 +0530

    form fillup function add

--- popup.js
+++ popup.js
@@ -48,3 +48,52 @@ clearBtn.addEventListener("click", () => {
     });
   }
 });
+
+//form fillup
+fillBtn.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  const dataToFill = {
+    name: userName.value,
+    email: userEmail.value,
+    phone: userPhone.value,
+    skills: userSkills.value,
+    experience: userExperience.value,
+  };
+
+  //scripting
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: injectFillLogic,
+    args: [dataToFill],
+  });
+});
+
+// from fillup logic
+function injectFillLogic(data) {
+  // common selectors
+  const selectors = {
+    name: ['input[name*="name" i]', 'input[id*="name" i]', 'input[placeholder*="name" i]'],
+    email: ['input[type="email" i]', 'input[name*="email" i]', 'input[id*="email" i]'],
+    phone: [
+      'input[type="tel" i]',
+      'input[name*="phone" i]',
+      'input[name*="mobile" i]',
+      'input[id*="phone" i]',
+    ],
+    skills: ['textarea[name*="skill" i]', 'textarea[id*="skill" i]', 'input[name*="skill" i]'],
+    experience: ['textarea[name*="exp" i]', 'textarea[id*="exp" i]', 'textarea[name*="about" i]'],
+  };
+
+  for (const key in selectors) {
+    for (const selector of selectors[key]) {
+      const element = document.querySelector(selector);
+      if (element && data[key]) {
+        element.value = data[key];
+        element.dispatchEvent(new Event("input", { bubbles: true }));
+        element.dispatchEvent(new Event("change", { bubbles: true }));
+        break;
+      }
+    }
+  }
+}
